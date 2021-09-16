cf065d966ca0def64f941436b4155f5b792473cd
Somdeep Jana <42708746+somdeepjana@users.noreply.github.com>
2021-09-16 16:27:45 +0530

    IPTreatmentPackageEntity Created

---
+++ IPTreatmentManagement.Models/Entites/IPTreatmentPackageEntity.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IPTreatmentManagement.Models.Entites
+{
+    public class IPTreatmentPackageEntity
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string TreatmentPackageName { get; set; }
+        [Required]
+        public string TestDetails { get; set; }
+        [Required]
+        public decimal Cost { get; set; }
+        [Required]
+        public int TreatmentDurationInDays { get; set; }
+
+    }
+}
