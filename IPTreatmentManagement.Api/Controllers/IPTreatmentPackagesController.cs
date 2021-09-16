a01bd7a0437f8a3453e5a7c765b04e53c9d5b851
Somdeep Jana <42708746+somdeepjana@users.noreply.github.com>
2021-09-16 17:57:17 +0530

    IPTreatmentPackagesController Created

---
+++ IPTreatmentManagement.Api/Controllers/IPTreatmentPackagesController.cs
@@ -0,0 +1,48 @@
+﻿using AutoMapper;
+using IPTreatmentManagement.Models.Dtos.Response;
+using IPTreatmentManagement.Models.RepositorieInterfaces;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IPTreatmentManagement.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class IPTreatmentPackagesController : ControllerBase
+    {
+        private readonly IMapper _mapper;
+        private readonly IIPTreatmentPackageRepository _iPTreatmentPackageRepository;
+
+        public IPTreatmentPackagesController(
+            IMapper mapper,
+            IIPTreatmentPackageRepository iPTreatmentPackageRepository
+            )
+        {
+            _mapper = mapper;
+            _iPTreatmentPackageRepository = iPTreatmentPackageRepository;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IPTreatmentPackageResponseDto[]>> GetAllIPTreatmentPackages()
+        {
+            var iPTreatmentPackages = await _iPTreatmentPackageRepository.GetAllAsync();
+
+            return _mapper.Map<IPTreatmentPackageResponseDto[]>(iPTreatmentPackages);
+        }
+
+        [HttpGet("{packageName}")]
+        public async Task<ActionResult<IPTreatmentPackageResponseDto>> GetIPTreatmentPackageByName(string packageName)
+        {
+            var iPTreatmentPackage = await _iPTreatmentPackageRepository.GetByNameAsync(packageName);
+
+            if (iPTreatmentPackage == null)
+                return NotFound();
+
+            return _mapper.Map<IPTreatmentPackageResponseDto>(iPTreatmentPackage);
+        }
+    }
+}
