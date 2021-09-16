52bcc53c64bf084ea1079a00f8eaef6424820fb6
Somdeep Jana <42708746+somdeepjana@users.noreply.github.com>
2021-09-16 16:56:01 +0530

    IPTreatmentPackage and ISpecialist Repositories are implimented

---
+++ IPTreatmentManagement.Models/RepositorieInterfaces/IIPTreatmentPackageRepository.cs
@@ -0,0 +1,15 @@
+﻿using IPTreatmentManagement.Models.Entites;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IPTreatmentManagement.Models.RepositorieInterfaces
+{
+    public interface IIPTreatmentPackageRepository
+    {
+        IEnumerable<IPTreatmentPackageEntity> GetAllAsync();
+        IPTreatmentPackageEntity GetByNameAsync();
+    }
+}
