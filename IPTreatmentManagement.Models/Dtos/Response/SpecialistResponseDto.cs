7f8c95be1281604679f43a2233509d2022f05cca
Somdeep Jana <42708746+somdeepjana@users.noreply.github.com>
2021-09-16 16:41:08 +0530

    SpecialistResponseDto Created

---
+++ IPTreatmentManagement.Models/Dtos/Response/SpecialistResponseDto.cs
@@ -0,0 +1,28 @@
+﻿using IPTreatmentManagement.Models.Entites;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IPTreatmentManagement.Models.Dtos.Response
+{
+    public class SpecialistResponseDto
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        public AilmentDomain AreaOfExpertise { get; set; }
+
+        [Required]
+        public int ExperienceInYears { get; set; }
+
+        [Required, DataType(DataType.PhoneNumber)]
+        public string ContactNumber { get; set; }
+    }
+}
