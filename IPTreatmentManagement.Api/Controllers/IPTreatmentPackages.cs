472dd66767ee96f0dcb321b5b243460c9143eb5c
Somdeep Jana <42708746+somdeepjana@users.noreply.github.com>
2021-09-16 16:10:26 +0530

    PPseudo enpoints defined

---
+++ IPTreatmentManagement.Api/Controllers/IPTreatmentPackages.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IPTreatmentManagement.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class IPTreatmentPackagesController : ControllerBase
+    {
+
+        [HttpGet]
+        public async Task<IActionResult> GetAllIPTreatmentPackages()
+        {
+            return Ok("all Treatment packages");
+        }
+
+        [HttpGet("{packageName}")]
+        public async Task<IActionResult> GetIPTreatmentPackageByName(string packageName)
+        {
+            return Ok("a package details");
+        }
+    }
+}
--- IPTreatmentManagement.Api/Startup.cs
+++ IPTreatmentManagement.Api/Startup.cs
@@ -9,7 +9,9 @@ using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Reflection;
 using System.Threading.Tasks;
 
 namespace IPTreatmentManagement.Api
@@ -31,6 +33,10 @@ namespace IPTreatmentManagement.Api
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "IPTreatmentManagement.Api", Version = "v1" });
+
+                var xmlCommentFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+                var cmlCommentsFullPath = Path.Combine(AppContext.BaseDirectory, xmlCommentFile);
+                c.IncludeXmlComments(cmlCommentsFullPath);
             });
         }
 
