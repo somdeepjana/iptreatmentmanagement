3bc8563a0ebac2e183d4a13c2fef6a34243680a4
Somdeep Jana <42708746+somdeepjana@users.noreply.github.com>
2021-09-16 16:47:56 +0530

    Automapper Mapping Profile added

---
+++ IPTreatmentManagement.Models/MappingPofile.cs
@@ -0,0 +1,21 @@
+﻿using AutoMapper;
+using IPTreatmentManagement.Models.Dtos.Response;
+using IPTreatmentManagement.Models.Entites;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IPTreatmentManagement.Models
+{
+    public class MappingPofile : Profile
+    {
+        public MappingPofile()
+        {
+            CreateMap<IPTreatmentPackageEntity, IPTreatmentPackageResponseDto>();
+
+            CreateMap<SpecialistEntity, SpecialistResponseDto>();
+        }
+    }
+}
