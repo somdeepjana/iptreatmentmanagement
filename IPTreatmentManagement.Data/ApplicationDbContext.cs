d4f08e4848523f160ba43307b8ca5d395990e54d
Somdeep Jana <42708746+somdeepjana@users.noreply.github.com>
2021-09-16 17:12:51 +0530

    ApplicationDbContext Created

---
+++ IPTreatmentManagement.Data/ApplicationDbContext.cs
@@ -0,0 +1,21 @@
+﻿using IPTreatmentManagement.Models.Entites;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IPTreatmentManagement.EFCore.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options): base(options)
+        {
+        }
+
+        public DbSet<IPTreatmentPackageEntity> IPTreatmentPackages { get; set; }
+        public DbSet<SpecialistEntity> Specialists { get; set; }
+
+    }
+}
