cc55f79728e7a2fdec413cc15a48df8192b07ed5
Somdeep Jana <42708746+somdeepjana@users.noreply.github.com>
2021-09-16 17:30:45 +0530

    SpecialistRepository Created

---
+++ IPTreatmentManagement.Data/Repositories/SpecialistRepository.cs
@@ -0,0 +1,23 @@
+﻿using IPTreatmentManagement.Models.Entites;
+using IPTreatmentManagement.Models.RepositorieInterfaces;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IPTreatmentManagement.EFCore.Data.Repositories
+{
+    public class SpecialistRepository : Repository, ISpecialistRepository
+    {
+        public SpecialistRepository(ApplicationDbContext context) : base(context)
+        {
+        }
+
+        public async Task<IEnumerable<SpecialistEntity>> GetAll()
+        {
+            return await _context.Specialists.ToListAsync();
+        }
+    }
+}
