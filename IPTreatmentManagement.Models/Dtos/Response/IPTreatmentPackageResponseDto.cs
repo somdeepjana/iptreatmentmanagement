d629f163b239efaec5ebb318f2e148a03c3bf091
Somdeep Jana <42708746+somdeepjana@users.noreply.github.com>
2021-09-16 16:30:46 +0530

    IPTreatmentPackageResponseDto Created

---
+++ IPTreatmentManagement.Models/Dtos/Response/IPTreatmentPackageResponseDto.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IPTreatmentManagement.Models.Dtos.Response
+{
+    public class IPTreatmentPackageResponseDto
+    {
+        [Required]
+        public string TreatmentPackageName { get; set; }
+        [Required]
+        public string TestDetails { get; set; }
+        [Required]
+        public decimal Cost { get; set; }
+        [Required]
+        public int TreatmentDurationInDays { get; set; }
+    }
+}
