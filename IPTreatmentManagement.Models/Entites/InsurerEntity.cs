11939266e0a6c5414ea51551415449b3b99f2a12
Koustav Mishra <mkk6160@gmail.com>
2021-09-17 23:26:59 +0530

    Insurer Entity Added

---
+++ IPTreatmentManagement.Models/Entites/InsurerEntity.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IPTreatmentManagement.Models.Entites
+{
+    public class InsurerEntity
+    {
+        public int Id { get; set; }
+        [Required]
+        public string InsurerName { get; set; }
+        public int IPTreatmentPackageEntityId { get; set; }
+        public IPTreatmentPackageEntity IPTreatmentPackageEntity { get; set; }
+        [Required]
+        public int InsurerAmountLimit { get; set; }
+        [Required]
+        public int DisbursementDuration { get; set; }
+    }
+}
