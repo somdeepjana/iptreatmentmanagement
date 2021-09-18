b1f963c0cf470b32fbf0cfc0ab8c9a6db0f865c7
Koustav Mishra <mkk6160@gmail.com>
2021-09-18 09:59:49 +0530

    Initiate Cliam Request Dto Added

---
+++ IPTreatmentManagement.Models/Dtos/Request/InitiateClaimRequestDto.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IPTreatmentManagement.Models.Dtos.Request
+{
+    public class InitiateClaimRequestDto
+    {
+        [Required]
+        public string PatientName { get; set; }
+        [Required]
+        public string TreatmentPackageName { get; set; }
+        [Required]
+        public string InsurerName { get; set; }
+    }
+}
