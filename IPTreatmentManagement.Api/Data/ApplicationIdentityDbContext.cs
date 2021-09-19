3d9aaab00a3d3f5ba27358058d5c0f97ff3bb343
Somdeep Jana <42708746+somdeepjana@users.noreply.github.com>
2021-09-19 15:39:32 +0530

    AdminUser model seed data and role created

---
+++ IPTreatmentManagement.Api/Data/ApplicationIdentityDbContext.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using IPTreatmentManagement.Api.Models.Entity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace IPTreatmentManagement.Api.Data
+{
+    public class ApplicationIdentityDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public ApplicationIdentityDbContext(DbContextOptions<ApplicationIdentityDbContext> options) : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+        }
+    }
+}
