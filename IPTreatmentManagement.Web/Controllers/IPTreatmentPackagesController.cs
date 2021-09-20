52051eff3afc1e1ff6464bbc4d95f0693a2130b9
Somdeep Jana <42708746+somdeepjana@users.noreply.github.com>
2021-09-20 18:31:52 +0530

    IPTreatmentPackages List view and controller created without implimentation

---
+++ IPTreatmentManagement.Web/Controllers/IPTreatmentPackagesController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IPTreatmentManagement.Web.Controllers
+{
+    public class IPTreatmentPackagesController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Details(string treatmentPackageName)
+        {
+            return View();
+        }
+    }
+}
