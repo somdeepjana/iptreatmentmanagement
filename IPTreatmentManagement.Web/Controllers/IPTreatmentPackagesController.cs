52453e6ed6d2293c1992c21cff19c3cb6b522e63
Somdeep Jana <42708746+somdeepjana@users.noreply.github.com>
2021-09-20 18:49:33 +0530

    IPTreatmentPackagesController temp test with seed  data

--- IPTreatmentManagement.Web/Controllers/IPTreatmentPackagesController.cs
+++ IPTreatmentManagement.Web/Controllers/IPTreatmentPackagesController.cs
@@ -3,14 +3,23 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using AutoMapper;
+using IPTreatmentManagement.Api.Seeds;
+using IPTreatmentManagement.Models.Dtos.Response;
 
 namespace IPTreatmentManagement.Web.Controllers
 {
     public class IPTreatmentPackagesController : Controller
     {
+        private readonly IMapper _mapper;
+
+        public IPTreatmentPackagesController(IMapper mapper)
+        {
+            _mapper = mapper;
+        }
         public IActionResult Index()
         {
-            return View();
+            return View(_mapper.Map<IPTreatmentPackageResponseDto[]>(new IPTreatmentPackageSeedData().GetAll));
         }
 
         public IActionResult Details(string treatmentPackageName)
--- IPTreatmentManagement.Web/Startup.cs
+++ IPTreatmentManagement.Web/Startup.cs
@@ -23,6 +23,8 @@ namespace IPTreatmentManagement.Web
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddAutoMapper(typeof(IPTreatmentManagement.Models.MappingPofile));
+
             services.AddControllersWithViews();
         }
 
