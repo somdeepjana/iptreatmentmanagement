d4af9ad57995b62ed92f5c575d9382fba73dfc0e
Somdeep Jana <42708746+somdeepjana@users.noreply.github.com>
2021-09-20 21:29:52 +0530

    Handeling Error response implimented in controller temporarily

--- IPTreatmentManagement.Web/Controllers/IPTreatmentPackagesController.cs
+++ IPTreatmentManagement.Web/Controllers/IPTreatmentPackagesController.cs
@@ -2,30 +2,51 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Text.Json;
 using System.Threading.Tasks;
 using AutoMapper;
 using IPTreatmentManagement.Models.ApiRepositoryInterface;
 using IPTreatmentManagement.Models.Dtos.Response;
+using IPTreatmentManagement.Models.OperationalModels;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Refit;
 
 namespace IPTreatmentManagement.Web.Controllers
 {
     public class IPTreatmentPackagesController : Controller
     {
+        private readonly ILogger<IPTreatmentPackagesController> _logger;
         private readonly IIPTreatmentPackageApiRepository _treatmentPackageApiRepository;
 
-        public IPTreatmentPackagesController(
+        public IPTreatmentPackagesController(ILogger<IPTreatmentPackagesController> logger,
             IIPTreatmentPackageApiRepository treatmentPackageApiRepository)
         {
+            _logger = logger;
             _treatmentPackageApiRepository = treatmentPackageApiRepository;
         }
         public async Task<IActionResult> Index()
         {
-            return View(await _treatmentPackageApiRepository.GetAll());
+            var treatmentPackages = await _treatmentPackageApiRepository.GetAll();
+            return View(treatmentPackages);
         }
 
         public async Task<IActionResult> Details(string treatmentPackageName)
         {
-            return View(await _treatmentPackageApiRepository.GetByPackageName(treatmentPackageName));
+            try
+            {
+                var treatmentPackage = await _treatmentPackageApiRepository.GetByPackageName(treatmentPackageName);
+                return View(treatmentPackage);
+            }
+            catch (ApiException ex)
+            {
+                var error = await ex.GetContentAsAsync<ErrorResponseModel>();
+                if (error.ApplicationStatusCode == (int) ApplicationStatusCodes.IPTreatmentPackageEntityNotFound)
+                    return NotFound(error);
+            }
+
+            return StatusCode(StatusCodes.Status500InternalServerError);
         }
     }
 }
