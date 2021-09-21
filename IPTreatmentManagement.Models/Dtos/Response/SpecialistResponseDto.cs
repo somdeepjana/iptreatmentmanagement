078ae26cde026f18d0583462946d92a07ca858ae
Somdeep Jana <42708746+somdeepjana@users.noreply.github.com>
2021-09-21 16:15:30 +0530

    Specialist display name implimented

--- IPTreatmentManagement.Models/Dtos/Response/SpecialistResponseDto.cs
+++ IPTreatmentManagement.Models/Dtos/Response/SpecialistResponseDto.cs
@@ -17,12 +17,15 @@ namespace IPTreatmentManagement.Models.Dtos.Response
         public string Name { get; set; }
 
         [Required]
+        [Display(Name = "Area Of Expertise")]
         public AilmentDomain AreaOfExpertise { get; set; }
 
         [Required]
+        [Display(Name = "Experience In Years")]
         public int ExperienceInYears { get; set; }
 
         [Required, DataType(DataType.PhoneNumber)]
+        [Display(Name = "Contact Number")]
         public string ContactNumber { get; set; }
     }
 }
